8b5681810b849902af9b68a059648b7841ffb140
William Aidan Good <aidangood5@gmail.com>
2020-03-08 13:51:42 -0400

    Add files via upload

--- pathmouserecurssion.py
+++ pathmouserecurssion.py
@@ -1,31 +1,34 @@
 ''' CSCI 204 P2 - Maze Solving using Recursive Algorithm
 Author: Aidan Good
-Using a recursive algorithim, finding all the exits to a maze.
-Displays the exit path as '!'
+Using a recursive algorithim, finding and numbering all the exits to a maze.
+Displays the exit path in order of the steps taken
 i.e.
 *******
-!!!!!!!
+0123456
 *******
 '''
 from maze import Maze
 import copy
 
-class Mouse:
+class PMouse:
     def __init__(self):
         self.num_unique_exits = 1
-        
 
     def find_maze_paths ( self, maze, s_row, s_col, e_row, e_col ):
         """ Function to run the recursive algorithim to find all possible exits
             out of the maze """
         
-        # Needed to be redefined with implimentation of numbered path
-        maze.THEWAYOUT = '!'
-
-        # Make copy of maze for recursive call so '!' markers are only on
-        # solved path
+        # Make copy of maze for recursive calls 
         maze_copy = copy.deepcopy( maze )
         maze_copy.theMaze[s_row][s_col] = maze.THEWAYOUT
+
+        # Tracking the step # while solving the maze
+        counter = int(maze_copy.THEWAYOUT)
+        counter += 1
+        if counter == 10:
+            counter = 0
+        maze_copy.THEWAYOUT = str( counter )
+        
 
         # Check to see if the exit tile is found, and return the map if it is.
         cur = [s_row, s_col]
@@ -33,12 +36,12 @@ class Mouse:
             print('-----Unique Exit', self.num_unique_exits, 'Found----- \n')
             print( maze_copy )
             self.num_unique_exits +=1
-
+            
         else:
             """ Check each direction to see if it is an open space. If it's open,
                 recursively call find_maze_paths() to run through each
                 possible path to see if the open tile leads to the exit tile."""
-            
+ 
             # Look Up
             if maze_copy.is_clear(s_row - 1, s_col) == True: 
                 self.find_maze_paths( maze_copy, s_row - 1, s_col, e_row, e_col )
@@ -55,11 +58,5 @@ class Mouse:
             if maze_copy.is_clear(s_row, s_col + 1) == True: 
                 self.find_maze_paths( maze_copy, s_row, s_col + 1, e_row, e_col )
 
-            # End recursive chain if no path is found
             else:
                 return
-
-        
-        
-
-            
--- testmouse.py
+++ testmouse.py
@@ -1,28 +1,52 @@
-#from mousestack import Mouse
+''' CSCI 204 P2 - Maze Solving using Recursive Algorithm
+Author: Aidan Good
+Have user select maze and path type to test out the recursive algorithim
+'''
 from mouserecursion import Mouse
+from pathmouserecurssion import PMouse
 from maze import Maze
 
-mouse = Mouse()
 maze = Maze()
 
-f_name = input( "Enter the name of the maze file (\"none\" if " \
-		     + " using random file): ")
+# Input name of maze file
+f_name = input( "\nEnter the name of the maze file \n(leave blank if " \
+		     + "you wish to generate a random maze): ")
 print( f_name )
 
-if f_name != 'none':
+# Try to read file. Generate random file if it fails to read.
+try:
     maze.read_maze( f_name )
+except:
+    print('-------- Could not open file --------')
+    print('-------- Generating Random Maze--------')
+    pass
 
+# Show user the maze, and ask for start and end tiles
 print( '-------- The Original Maze --------' )
 print( maze )
 
 starting_row = int( input( 'Please enter the starting row : ' ) )
-print( starting_row )
 starting_col = int( input( 'Please enter the starting column : ' ) )
-print( starting_col )
 
 exit_row = int( input( 'Please enter the exiting row : ' ) )
-print( exit_row )
 exit_col = int( input( 'Please enter the exiting column : ' ) )
-print( exit_col )
 
-mouse.find_maze_paths( maze, starting_row, starting_col, exit_row, exit_col )
+
+# Ask the user what type of path markers they would like
+print("\nThe default path marker is '!'")
+path_type = input("\nTo have the steps be numbered instead, type in 'numpath'."\
+                   + "\nOtherwise leave blank for the default path: ")
+
+# Check if user wanted to use numbered path markers
+if path_type == 'numpath':
+    mouse = PMouse()
+    print("\n------Using numbered path------")
+    print("---Steps taken are shown, leading from starting step '0' to end step---")
+    mouse.find_maze_paths( maze, starting_row, starting_col, exit_row, exit_col )
+
+# defaults to '!' markers 
+else:
+    mouse = Mouse()
+    print("\n------Using default '!' path------")
+    print('------Steps taken are shown------')
+    mouse.find_maze_paths( maze, starting_row, starting_col, exit_row, exit_col )
